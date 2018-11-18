5351e69857a04d064ba904a52783fe3a8b9a06ff
Fabian Otto <fabi.otto@t-online.de>
2018-11-18 13:21:19 +0100

    GP and NN models for dynamics and reward

---
+++ Challenge_1/DataGenerator.py
@@ -0,0 +1,61 @@
+import gym
+import numpy as np
+
+
+class DataGenerator():
+
+    def __init__(self, env_name, seed):
+        self.env_name = env_name
+        self.seed = seed
+
+        self.env = gym.make(self.env_name)
+        self.env.seed(self.seed)
+
+        if env_name == "Pendulum-v0":
+            self.n_samples = 10000
+        elif env_name == "Qube-v0":
+            self.n_samples = 25000
+        else:
+            raise NotImplementedError("Unsupported Environment")
+
+        # get the number of available action from the environment
+        self.state_dim = self.env.observation_space.shape[0]
+        self.n_actions = self.env.action_space.shape[0]
+
+    def get_samples(self, n_samples=None):
+        """
+        returns n samples in the form s', s, a, r
+        :param n_samples:
+        :return: s', s, a, r
+        """
+
+        if n_samples is not None:
+            self.n_samples = n_samples
+
+        # sample dataset with random actions
+        states = np.zeros((self.n_samples, self.state_dim))
+        states_prime = np.zeros((self.n_samples, self.state_dim))
+        actions = np.zeros((self.n_samples,))
+        rewards = np.zeros((self.n_samples,))
+
+        i = 0
+        while i < self.n_samples:
+            state = self.env.reset()
+            done = False
+
+            while not done and i < self.n_samples:
+                # sample and apply action
+                action = self.env.action_space.sample()
+                state_prime, reward, done, _ = self.env.step(action)
+
+                # save outcomes
+                states_prime[i] = np.array(state_prime)
+                states[i] = np.array(state)
+                actions[i] = action
+                rewards[i] = reward
+
+                # increment counter
+                state = state_prime
+                i += 1
+
+        return states_prime, states, actions, rewards
---
+++ Challenge_1/main.py
@@ -0,0 +1,45 @@
+import numpy as np
+import sklearn
+
+from Challenge_1.DataGenerator import DataGenerator
+from Challenge_1.ModelsGP import GPModel
+
+seed = 123
+n_samples_train = 100
+n_samples_test = 100
+
+env_name = "Pendulum-v0"
+# env_name = "Qube-v0"
+
+dg_train = DataGenerator(env_name=env_name, seed=seed)
+s_prime, s, a, r = dg_train.get_samples(n_samples_train)
+
+# create training input pairs
+s_a_pairs = np.concatenate([s, a[:, np.newaxis]], axis=1)
+
+# solve regression problem s_prime = f(s,a)
+dynamics_model = GPModel()
+dynamics_model.fit(s_a_pairs, s_prime)
+
+# solve regression problem r = g(s,a)
+reward_model = GPModel()
+reward_model.fit(s_a_pairs, r)
+
+# --------------------------------------------------------------
+# compute accuracy on test set
+
+dg_test = DataGenerator(env_name=env_name, seed=seed)
+s_prime_test, s_test, a_test, r_test = dg_test.get_samples(n_samples_test)
+
+# create test input pairs
+s_a_pairs_test = np.concatenate([s_test, a_test[:, np.newaxis]], axis=1)
+
+# make prediction for dynamics model
+s_a_pred = dynamics_model.predict(s_a_pairs_test)
+# make prediction for reward model
+reward_pred = reward_model.predict(s_a_pairs_test)
+
+acc_dynamics = sklearn.metrics.accuracy_score(s_prime_test, s_a_pred)
+acc_reward = sklearn.metrics.accuracy_score(r_test, reward_pred)
+print("Accuracy for dynamics model: {}".format(acc_dynamics))
+print("Accuracy for reward model: {}".format(acc_reward))
