d24469a194cffeb68280e8fdc79c62f3c791572e
BoboDance <fabi.otto@t-online.de>
2018-12-15 17:05:45 +0100

    not enough memory for grid search of qube

--- Challenge_1/Custom_env/__init__.py
+++
@@ -1,7 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='PendulumCustom-v0',
-    entry_point='Challenge_1.Custom_env.PendulumEnvCustom:PendulumEnvCustom',
-    max_episode_steps=200,
-)
---
+++ Challenge_1/README.md
@@ -0,0 +1,7 @@
+### Challenge 1
+
+Best results for Pendulum-v2
+
+100 epochs: Mean reward: -147.71839019523438 -- standard deviation of rewards: 100.89982163365936
+
+[Sourcecode](./main.py#L10)
