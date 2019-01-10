cd2f80493c0e4ad53927c355c16618055b5f8086
tomatenbrei <tomadroide@gmail.com>
2019-01-10 14:01:14 +0100

    Fixed initial sample creation

--- Challenge_2/Common/Util.py
+++ Challenge_2/Common/Util.py
@@ -5,11 +5,16 @@ def create_initial_samples(env, memory, count, discrete_actions):
     last_observation = env.reset()
     samples = 0
     while samples < count:
+        # choose a random action
         action_idx = np.random.choice(range(len(discrete_actions)), 1)
         action = discrete_actions[action_idx]
+        # do the step
         observation, reward, done, _ = env.step(action)
+
         memory.push((*last_observation, *action_idx, reward, *observation, done))
+
         samples += 1
+        last_observation = observation
 
         if done:
             last_observation = env.reset()
