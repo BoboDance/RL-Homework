2919a8d320f12037c53c30ee02d9612379682220
BoboDance <fabi.otto@t-online.de>
2019-01-27 12:56:28 +0100

    fixed hyperparams for DQN

--- Challenge_2/DQN/CartpoleSwingShortTest.py
+++ Challenge_2/DQN/CartpoleSwingShortTest.py
@@ -6,7 +6,7 @@ from torch.optim.lr_scheduler import StepLR, CosineAnnealingLR
 from Challenge_2.Common.Util import evaluate
 from Challenge_2.DQN.DQN import DQN
 from Challenge_2.DQN.DQNSwingShortModel import DQNSwingShortModel
-from Challenge_2.DQN.Util import get_policy_fun
+from Challenge_2.DQN.Util import get_policy_fun, save_model
 from torch import nn
 
 seed = 1
@@ -19,7 +19,7 @@ env.seed(seed)
 # Golden Hyperparameters
 min_action = -5
 max_action = 5
-nb_bins = 7
+nb_bins = 5
 lr = 1e-4
 memory_size = int(1e6)
 gamma = 0.99
@@ -29,16 +29,17 @@ eps_decay = 1e5  # they divided epsilon by 1.01 after every episode
 max_episodes = 300
 max_episode_length = 1e4
 minibatch_size = 1000
+target_model_update_steps = 3000
 optimizer = "adam"
 lr_scheduler = None
 loss = nn.SmoothL1Loss()
 normalize = False
-anti_sucide = True
+anti_sucide = False
 edge_fear_threshold = 0.3
 
 discrete_actions = np.linspace(min_action, max_action, nb_bins)
 
-Q = DQNSwingShortModel(env, discrete_actions, optimizer="adam", lr=lr)
+Q = DQNSwingShortModel(env, discrete_actions, optimizer=optimizer, lr=lr)
 
 # test for dividing lr by 2 every 50 episodes - start_lr = 1e-3 - noramlize=False
 # Stats: Episodes 100, avg: 932.2933493737411, std: 22.219861482005673
@@ -46,7 +47,7 @@ Q = DQNSwingShortModel(env, discrete_actions, optimizer="adam", lr=lr)
 # lr_scheduler = CosineAnnealingLR(Q.optimizer, 10) #T_max=max_episode_length)
 
 dqn = DQN(env, Q, memory_size=memory_size, initial_memory_count=minibatch_size, minibatch_size=minibatch_size,
-          target_model_update_steps=minibatch_size, gamma=gamma,
+          target_model_update_steps=target_model_update_steps, gamma=gamma,
           eps_start=eps_start, eps_end=eps_end, eps_decay=eps_decay, max_episodes=max_episodes,
           max_steps_per_episode=max_episode_length, lr_scheduler=lr_scheduler, loss=loss, normalize=normalize)
 
@@ -57,6 +58,6 @@ trained_episodes = dqn.train()
 Q.load_state_dict(torch.load("./checkpoints/best_weights.pth"))
 
 eval_reward_mean = evaluate(env, get_policy_fun(Q), episodes=100, render=5)
-#save_model(env, Q, trained_episodes, eval_reward_mean)
+save_model(env, Q, trained_episodes, eval_reward_mean)
 
 env.close()
