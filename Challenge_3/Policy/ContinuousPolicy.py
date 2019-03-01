6f338c3ccd334a550c9669e011b4489face0c3c0
tomatenbrei <tomadroide@gmail.com>
2019-03-01 09:20:01 +0100

    You can now choose whether you want state-dependent sigma in the continuous policy

--- Challenge_3/Policy/ContinuousPolicy.py
+++ Challenge_3/Policy/ContinuousPolicy.py
@@ -5,12 +5,13 @@ from torch.distributions import Normal
 
 
 class ContinuousPolicy(nn.Module):
-    def __init__(self, env, n_hidden_units=16):
+    def __init__(self, env, n_hidden_units=16, state_dependent_sigma=False):
         """
         Create a continuous policy which samples its actions using predicted a gaussian for a given observation.
 
         :param env: Gym environment object for which this policy object is created
         :param n_hidden_units: Number of nodes in the hidden layer
+        :param state_dependent_sigma: Predict sigma together with the mean or use the same sigma for every observation
         """
 
         super(ContinuousPolicy, self).__init__()
@@ -21,8 +22,12 @@ class ContinuousPolicy(nn.Module):
 
         self.linear_input_to_hidden = nn.Linear(self.n_inputs, n_hidden_units)
         self.linear_hidden_to_mean = nn.Linear(n_hidden_units, self.n_actions)
-        # self.linear_hidden_to_sigma = nn.Linear(n_hidden_units, self.n_actions)
-        self.sigma = nn.Parameter(torch.zeros(self.n_actions))
+
+        self.state_dependent_sigma = state_dependent_sigma
+        if self.state_dependent_sigma:
+            self.linear_hidden_to_sigma = nn.Linear(n_hidden_units, self.n_actions)
+        else:
+            self.sigma = nn.Parameter(torch.zeros(self.n_actions))
 
         self.train()
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -36,10 +41,13 @@ class ContinuousPolicy(nn.Module):
         """
         hidden = F.relu(self.linear_input_to_hidden(inputs))
         means = self.linear_hidden_to_mean(hidden)
-        # sigma = self.linear_hidden_to_sigma(hidden)
-        # sigma = F.softplus(sigma)
 
-        return means, F.softplus(self.sigma)
+        if self.state_dependent_sigma:
+            sigma = F.softplus(self.linear_hidden_to_sigma(hidden))
+        else:
+            sigma = F.softplus(self.sigma)
+
+        return means, sigma
 
     def choose_action_by_sampling(self, observation):
         """
